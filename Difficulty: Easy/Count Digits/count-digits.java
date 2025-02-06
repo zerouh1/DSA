294973ce5efa54cbb174f624fad0a2d3b29134f6
Sarthak Khandelwal <145242440+zerouh1@users.noreply.github.com>
2025-02-06 08:24:39 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Count Digits/count-digits.java
@@ -0,0 +1,38 @@
+//{ Driver Code Starts
+// Initial Template for Java
+
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) throws IOException {
+        BufferedReader read = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(read.readLine());
+        while (t-- > 0) {
+            int N = Integer.parseInt(read.readLine());
+            Solution ob = new Solution();
+            System.out.println(ob.evenlyDivides(N));
+
+            System.out.println("~");
+        }
+    }
+}
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+class Solution {
+    static int evenlyDivides(int n) {
+        int original = n;
+        int count = 0;
+        while(n>0){
+            int temp = n%10;
+            if(temp!=0&&original%temp == 0){
+                count++;
+            }
+            n /= 10;
+        }
+        return count;
+    }
+}
