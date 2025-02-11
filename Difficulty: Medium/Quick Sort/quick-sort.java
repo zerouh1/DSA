7a08d204f6aed74345522aa79ecdb4901c5da64c
Sarthak Khandelwal <145242440+zerouh1@users.noreply.github.com>
2025-02-11 22:40:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Quick Sort/quick-sort.java
@@ -0,0 +1,68 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.util.*;
+
+class Sorting {
+    static void printArray(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n; ++i) System.out.print(arr[i] + " ");
+        System.out.println();
+    }
+
+    // Driver program
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t = Integer.parseInt(br.readLine());
+        while (t-- > 0) {
+            String inputLine[] = br.readLine().trim().split(" ");
+            int n = inputLine.length;
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputLine[i]);
+            }
+
+            new Solution().quickSort(arr, 0, n - 1);
+            printArray(arr);
+        }
+    }
+}
+// } Driver Code Ends
+
+
+class Solution {
+    // Function to sort an array using quick sort algorithm.
+    static void quickSort(int arr[], int low, int high) {
+        // code here
+        if (low < high) {
+            // Find the partition index
+            int pivotIndex = partition(arr, low, high);
+            
+            // Recursively sort elements before and after partition
+            quickSort(arr, low, pivotIndex - 1);
+            quickSort(arr, pivotIndex + 1, high);
+        }
+    }
+
+    static int partition(int arr[], int low, int high) {
+        // your code here
+        int pivot = arr[high]; // Choosing the last element as pivot
+        int i = low - 1; // Index of smaller element
+
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                i++;
+                // Swap arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+        
+        // Swap pivot element with element at (i + 1)
+        int temp = arr[i + 1];
+        arr[i + 1] = arr[high];
+        arr[high] = temp;
+        
+        return i + 1;
+    }
+}
